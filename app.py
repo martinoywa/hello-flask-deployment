cbaf532d46174200b2bc521ddbe1d3e3fd8bb18f
Martin Oywa <martinoywa2@gmail.com>
2022-06-23 08:26:41 +0000

    update app.py file

--- app.py
+++ app.py
@@ -1,2 +1,17 @@
-def say_hello():
-    return "Hello Human!"
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+app.route("/")
+def index():
+    return "Hello Flask"
+
+app.route("/greet/<name>")
+def greet(name):
+    return f"Hello {name}"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
